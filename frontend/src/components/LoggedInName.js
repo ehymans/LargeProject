5812bb7ac359e7c640f65bd4817e6c91bb2c4596
Ethan Hymans <ethanhymans@gmail.com>
2023-10-12 16:42:43 -0400

    changed so it does not display 'john doe' as the login name

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -1,3 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+function LoggedInName() {
+
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        let _ud = localStorage.getItem('user_data');
+        if (_ud) {
+            let ud = JSON.parse(_ud);
+            setUser(ud);
+        }
+    }, []);
+
+    const doLogout = event => {
+        event.preventDefault();
+        localStorage.removeItem('user_data'); // Clear the user data upon logout
+        window.location.href = '/'; // Redirect to login page
+        alert('Logged out successfully');
+    };
+
+    return (
+        <div id="loggedInDiv">
+            <span id="userName">Logged In As {user.firstName} {user.lastName} </span><br />
+            <button type="button" id="logoutButton" class="buttons" onClick={doLogout}> Log Out </button>
+        </div>
+    );
+};
+
+export default LoggedInName;
+
+/*
 import React from 'react';
 
 function LoggedInName()
@@ -21,4 +53,4 @@ function LoggedInName()
     );
 };
 
-export default LoggedInName;
+export default LoggedInName;*/
