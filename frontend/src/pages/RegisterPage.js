7b4392828fd00dab4f2b8ef6d749cb3125b3e2aa
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-10-17 13:23:13 -0400

    Add files via upload

---
+++ frontend/src/pages/RegisterPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTitle from '../components/PageTitle';
+import RegisterForm from '../components/Register'; 
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <PageTitle />
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
