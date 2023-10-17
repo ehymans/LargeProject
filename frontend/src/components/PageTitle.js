190d559e5c9e9d573ace24e829cc28cca98c336c
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-10-17 13:26:47 -0400

    Update PageTitle.js

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
 
-function PageTitle()
-{
-   return(
-     <h1 id="title">LARGE PROJECT</h1>
-   );
-};
+function PageTitle() {
+  return (
+    <div className="page-title">
+      <h1 id="title">LARGE PROJECT</h1>
+      <div className="page-buttons">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Register</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default PageTitle;
