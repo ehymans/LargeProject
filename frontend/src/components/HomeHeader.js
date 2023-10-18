16b03a655f21ccd13ca6e649ee1272dcc0709ab3
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-10-18 19:08:12 -0400

    Add files via upload

--- frontend/src/components/HomeHeader.js
+++ frontend/src/components/HomeHeader.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for navigation
-import './PageTitle.css';
-
-function LoggedInName() {
-    const [user, setUser] = useState({});
-  
-    useEffect(() => {
-      let _ud = localStorage.getItem('user_data');
-      if (_ud) {
-        let ud = JSON.parse(_ud);
-        setUser(ud);
-      }
-    }, []);
-  
-    const doLogout = event => {
-      event.preventDefault();
-      localStorage.removeItem('user_data'); // Clear the user data upon logout
-      window.location.href = '/'; // Redirect to the login page
-      alert('Logged out successfully');
-    };
+// HomeHeader.js
+import React, { useState, useEffect } from 'react';
+import './PageTitle';
 
 function HomeHeader() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    let _ud = localStorage.getItem('user_data');
+    if (_ud) {
+      let ud = JSON.parse(_ud);
+      setUser(ud);
+    }
+  }, []);
+
+  const doLogout = event => {
+    event.preventDefault();
+    localStorage.removeItem('user_data'); // Clear the user data upon logout
+    window.location.href = '/'; // Redirect to the login page
+    alert('Logged out successfully');
+  };
+
   return (
-    <div className="page-title">
+    <div className="home-header">
       <h1 id="title">Productivity Dashboard</h1>
       <span id="userName">Logged In As {user.firstName} {user.lastName}</span><br />
       <div className="page-buttons">
-        
-          <button>onClick={doLogout}Login</button>
+        <button onClick={doLogout}>Logout</button>
       </div>
     </div>
   );
-}
 }
 
 export default HomeHeader;
