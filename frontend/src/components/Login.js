c6e2a759ee738d014301f7d8e782e6c12e459df0
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-18 20:24:01 -0400

    Fixing...

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,63 +7,6 @@ import './Login.css';
 // Import or define buildPath
 import { buildPath } from './Path.js'; // Assuming 'buildPath' is a function you need to import
 
-function Login() {
-  var loginName;
-  var loginPassword;
-  const [message, setMessage] = useState('');
-
-  const doLogin = async event => {
-    event.preventDefault();
-
-    var obj = { login: loginName.value, password: loginPassword.value };
-    var js = JSON.stringify(obj);
-
-    try {
-      const response = await fetch(buildPath('api/login'), {
-        method: 'POST',
-        body: js,
-        headers: { 'Content-Type': 'application/json' },
-      });
-
-      var res = JSON.parse(await response.text());
-      if (res.id <= 0) {
-        setMessage('User/Password combination incorrect');
-      } else {
-        var user = { firstName: res.firstName, lastName: res.lastName, id: res.id };
-        localStorage.setItem('user_data', JSON.stringify(user));
-
-        setMessage('');
-        window.location.href = '/home'; /// sends the user to the home page of the website
-      }
-    } catch (e) {
-      // setMessage("Error is here!!");
-      alert(e.toString());
-      return;
-    }
-  };
-
-  return (
-    <div className="background-container">
-      <div id="loginDiv">
-        <form onSubmit={doLogin} className="login-form">
-          <h2 className="form-title">PLEASE LOG IN</h2>
-          <div className="form-group">
-            <input type="text" id="loginName" placeholder="Username" ref={(c) => (loginName = c)} />
-          </div>
-          <div className="form-group">
-            <input type="password" id="loginPassword" placeholder="Password" ref={(c) => (loginPassword = c)} />
-          </div>
-          <button type="submit" id="loginButton" className="login-button" onClick={doLogin}>
-            Do It
-          </button>
-        </form>
-        <p id="loginResult" className="login-message">
-          {message}
-        </p>
-      </div>
-    </div>
-  );
-}
 
 function Login() {
   var loginName;
