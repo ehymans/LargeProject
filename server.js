53009c86c50f6789a323ddbeaebed6363f0bf8f9
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-18 22:10:12 -0400

    fixing CORS issue.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json());
 
 require('dotenv').config();
@@ -31,7 +31,7 @@ app.use((req, res, next) =>
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS, fetch'
+    'GET, POST, PATCH, DELETE, OPTIONS'
   );
   next();
 });
