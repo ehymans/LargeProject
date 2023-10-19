8e68a72bee6397f4a7a3f4c5e33e9ad42221f6ed
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-18 20:01:39 -0400

    Script for login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,81 +1,68 @@
-//import React from 'react';
 import React, { useState } from 'react';
 import './Login.css';
 
-const app_name = 'progress-tracker-4331-88c53c23c126'
+// Remove unused variables
+// const app_name = 'progress-tracker-4331-88c53c23c126';
 
-function buildPath(route)
-{
-    if (process.env.NODE_ENV === 'production') 
-    {
-        return 'https://' + app_name +  '.herokuapp.com/' + route;
+// Import or define buildPath
+import { buildPath } from './Path.js'; // Assuming 'buildPath' is a function you need to import
+
+function Login() {
+  var loginName;
+  var loginPassword;
+  const [message, setMessage] = useState('');
+
+  const doLogin = async event => {
+    event.preventDefault();
+
+    var obj = { login: loginName.value, password: loginPassword.value };
+    var js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch(buildPath('api/login'), {
+        method: 'POST',
+        body: js,
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      var res = JSON.parse(await response.text());
+      if (res.id <= 0) {
+        setMessage('User/Password combination incorrect');
+      } else {
+        var user = { firstName: res.firstName, lastName: res.lastName, id: res.id };
+        localStorage.setItem('user_data', JSON.stringify(user));
+
+        setMessage('');
+        window.location.href = '/home'; /// sends the user to the home page of the website
+      }
+    } catch (e) {
+      // setMessage("Error is here!!");
+      alert(e.toString());
+      return;
     }
-    else
-    {        
-        return 'http://localhost:5000/' + route;
-    }
-}
+  };
 
-function Login()
-{
-    var loginName;
-    var loginPassword;
-    const [message, setMessage] = useState('');
-
-
-        const doLogin = async event => 
-        {
-            event.preventDefault();
-    
-            var obj = {login:loginName.value,password:loginPassword.value};
-            var js = JSON.stringify(obj);
-    
-            try
-            {
-
-                const response = await fetch(buildPath('api/login'),{method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
-
-
-                var res = JSON.parse(await response.text());
-                if( res.id <= 0 )
-                {
-                    setMessage('User/Password combination incorrect');
-                }
-                else
-                {
-                    var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
-                    localStorage.setItem('user_data', JSON.stringify(user));
-    
-                    setMessage('');
-                    window.location.href = '/home';            /// sends the user to the home page of the website
-                }
-            }
-            catch(e)
-            {
-                //setMessage("Error is here!!");
-                alert(e.toString());
-                return;
-            }
-        };
-        
-    
-    return(
-        <div class="background-container">
-        <div id="loginDiv">
-        <form onSubmit={doLogin} class="login-form">
-          <h2 class="form-title">PLEASE LOG IN</h2>
-          <div class="form-group">
-            <input type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} />
+  return (
+    <div className="background-container">
+      <div id="loginDiv">
+        <form onSubmit={doLogin} className="login-form">
+          <h2 className="form-title">PLEASE LOG IN</h2>
+          <div className="form-group">
+            <input type="text" id="loginName" placeholder="Username" ref={(c) => (loginName = c)} />
           </div>
-          <div class="form-group">
-            <input type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} />
+          <div className="form-group">
+            <input type="password" id="loginPassword" placeholder="Password" ref={(c) => (loginPassword = c)} />
           </div>
-          <button type="submit" id="loginButton" class="login-button" onClick={doLogin}>Do It</button>
+          <button type="submit" id="loginButton" className="login-button" onClick={doLogin}>
+            Do It
+          </button>
         </form>
-        <p id="loginResult" class="login-message">{message}</p>
+        <p id="loginResult" className="login-message">
+          {message}
+        </p>
       </div>
-      </div>
-    );
-};
+    </div>
+  );
+}
 
 export default Login;
