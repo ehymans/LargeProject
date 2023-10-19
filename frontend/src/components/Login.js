20889033bbb88fbe5e937b703e72b820b2493be9
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-18 21:24:02 -0400

    Fixing Login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -20,8 +20,9 @@ function Login() {
       var js = JSON.stringify(obj);
 
       try {
-          const response = await fetch(buildPath('api/login'), { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
-
+        var bp = require('./Path.js');
+        const response = await fetch(bp.buildPath('api/login'),
+        {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
           var res = JSON.parse(await response.text());
           if (res.id <= 0) {
               setMessage('User/Password combination incorrect');
