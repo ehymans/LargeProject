cb86e302078b1a8386c705869d6aa3a27e4f91f9
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-19 02:36:25 -0400

    Register work

--- api.js
+++ api.js
@@ -50,7 +50,7 @@ exports.setApp = function (app , client)
       });
       
     app.post('/api/register', async (req, res, next) => {
-      console.log("inside register api.")
+      console.log("inside register api.");
       // Incoming: first name, last name, username, password
       const { firstName, lastName, username, password, jwtToken } = req.body;
       
@@ -76,7 +76,7 @@ exports.setApp = function (app , client)
           console.log(error.message);
         } 
         var ret = { error: error };
-        console.log("done register api.")
+        console.log("done register api.");
         res.status(200).json(ret);
     });
       
