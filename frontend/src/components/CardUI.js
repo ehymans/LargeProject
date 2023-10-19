a53f379d57461b86d1bbf29940a0e480f8764abd
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-18 20:48:07 -0400

    Fixing 5

--- frontend/src/components/CardUI.js
+++ frontend/src/components/CardUI.js
@@ -1,19 +1,11 @@
+var bp = require('./Path.js');
+const response = await fetch(bp.buildPath('api/login'),
+{method:'POST',body:js,headers:{'Content-Type': 'application/json'}})
+
 //import React from 'react';
 import React, { useState } from 'react';
 
 const app_name = 'progress-tracker-4331-88c53c23c126'
-
-function buildPath(route)
-{
-    if (process.env.NODE_ENV === 'production') 
-    {
-        return 'https://' + app_name +  '.herokuapp.com/' + route;
-    }
-    else
-    {        
-        return 'http://localhost:5000/' + route;
-    }
-}
 
 function CardUI()
 {
--- server.js
+++ server.js
@@ -18,88 +18,11 @@ const url = process.env.MONGODB_URI;
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 client.connect();
-
+var api = require('./api.js');
+api.setApp( app, client );
 // edit so it does not await the API call.
-app.post('/api/addcard', (req, res, next) => {
-    // incoming: userId, card
-    // outgoing: error
-    
-    const { userId, card } = req.body;
-  
-    const newCard = {Card:card,UserId:userId};
-    var error = '';
-  
-    try {
-      const db = client.db('COP4331Cards');
-      db.collection('Cards').insertOne(newCard);
-    }
-    catch(e) {
-      error = e.toString();
-    }
-  
-    //cardList.push(card);
-  
-    var ret = { error: error };
-    res.status(200).json(ret);
-  });
-  
 
-
-app.post('/api/login', async (req, res, next) => 
-{
-  // incoming: login, password
-  // outgoing: id, firstName, lastName, error
-	
- var error = '';
-
-  const { login, password } = req.body;
-
-  const db = client.db('LargeProject');
-  const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-
-  var id = -1;
-  var fn = '';
-  var ln = '';
-
-  if( results.length > 0 )
-  {
-    id = results[0].UserId;
-    fn = results[0].FirstName;
-    ln = results[0].LastName;
-  }
-
-  var ret = { id:id, firstName:fn, lastName:ln, error:''};
-  res.status(200).json(ret);
-});
-
-
-app.post('/api/searchcards', async (req, res, next) => 
-{
-  // incoming: userId, search
-  // outgoing: results[], error
-
-  var error = '';
-
-  const { userId, search } = req.body;
-
-  var _search = search.trim();
-  
-  const db = client.db('COP4331Cards');
-  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'i'}}).toArray();
-  
-  var _ret = [];
-  for( var i=0; i<results.length; i++ )
-  {
-    _ret.push( results[i].Card );
-  }
-  
-  var ret = {results:_ret, error:error};
-  res.status(200).json(ret);
-});
-
-
-
-app.use((req, res, next) => 
+app.use(async (req, res, next) => 
 {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
