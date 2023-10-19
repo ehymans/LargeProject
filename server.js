c1cad22b23c2284f6597fb1ccd4a889bbaccfb0a
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-19 00:37:47 -0400

    Finishing MERN C

--- server.js
+++ server.js
@@ -27,19 +27,19 @@ api.setApp( app, client );
 // edit so it does not await the API call.
 console.log("here2");
 
-app.use((req, res, next) => 
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
+//app.use((req, res, next) => 
+//{
+//  res.setHeader('Access-Control-Allow-Origin', '*');
+//  res.setHeader(
+//    'Access-Control-Allow-Headers',
+//    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//  );
+//  res.setHeader(
+//    'Access-Control-Allow-Methods',
+//    'GET, POST, PATCH, DELETE, OPTIONS'
+//  );
+//  next();
+//});
 
 app.listen(PORT, () => 
 {
