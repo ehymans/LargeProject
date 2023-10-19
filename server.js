255e75164caadcb6462bdc819e760e273c5f9eff
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-18 22:06:59 -0400

    Fixing more issues with login and register

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use((req, res, next) =>
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS, FETCH'
+    'GET, POST, PATCH, DELETE, OPTIONS, fetch'
   );
   next();
 });
