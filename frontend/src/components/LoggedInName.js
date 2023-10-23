4d1cddc16585ca0c3b128a5fc86bf373f159db98
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-10-23 18:36:06 -0400

    Update LoggedInName.js

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -26,43 +26,12 @@ function LoggedInName() {
 
   return (
     <div id="loggedInDiv">
-      <span id="userName">Logged In As {user.firstName} {user.lastName}</span><br />
+     
       <button type="button" id="logoutButton" className="buttons" onClick={addExperience}>
         Add Exp</button>
      
 
-      {/* Add the blank table here */}
-      <table
-  style={{
-    border: '.5px solid #000',
-    margin: '0 auto', // Center the table horizontally
-    marginTop: '20px', // Add some top margin for spacing
-    padding: '10px', // Add padding for better visual separation
-  }}
->
-        <thead>
-          <tr>
-            <th>Task</th>
-            <th>Date Created</th>
-            <th>Due Date</th>
-            <th>Task Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Sample Task</td>
-            <td>1/1/1111</td>
-            <td>1/2/1111</td>
-            <td>Completed</td>
-          </tr>
-          <tr>
-          <td>Sample Task 2</td>
-            <td>1/4/1111</td>
-            <td>1/5/1111</td>
-            <td>Completed</td>
-          </tr>
-        </tbody>
-      </table>
+      
     </div>
   );
 }
