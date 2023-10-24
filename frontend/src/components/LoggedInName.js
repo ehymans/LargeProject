479b93efbc3272cbbc7de77a74428b2b9002a4f0
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-23 23:11:55 -0400

    Messing with the circular exp bar

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './HomePage.css';
 
-
 function LoggedInName() {
   const [user, setUser] = useState({});
+  const [progress, setProgress] = useState(0); // Add progress state
 
   useEffect(() => {
     let _ud = localStorage.getItem('user_data');
@@ -15,30 +15,37 @@ function LoggedInName() {
 
   const doLogout = event => {
     event.preventDefault();
-    localStorage.removeItem('user_data'); // Clear the user data upon logout
-    window.location.href = '/'; // Redirect to the login page
+    localStorage.removeItem('user_data');
+    window.location.href = '/';
     alert('Logged out successfully');
   };
+
   const addExperience = () => {
-    // For you to do Ollie.
-    alert('Add this endpoint - Julian');
+    // Update progress here
+    setProgress(progress + 10);
   };
 
-  const addTask = () =>{
-    //NEW API FOR TASK ADD
-    alert('Add this endpoint for task add');
+  const addTask = () => {
+    // Add your task-related code here
   };
 
   return (
     <div id="loggedInDiv">
-     
-      <button type="button" id="addExp" className="buttons" onClick={addExperience}>
-        Add Exp</button>
-        <button type="button" id= "addTask" className="buttons" onClick={addTask}>
-          Add Task</button>
-     
+      <span id="userName">{user.name}</span>
 
-      
+      <div id="progress-bar">
+        <div
+          id="progress-fill"
+          style={{ transform: `rotate(${progress * 3.6}deg)` }}
+        ></div>
+      </div>
+
+      <button type="button" id="addExp" className="buttons" onClick={addExperience}>
+        Add Exp
+      </button>
+      <button type="button" id="addTask" className="buttons" onClick={addTask}>
+        Add Task
+      </button>
     </div>
   );
 }
