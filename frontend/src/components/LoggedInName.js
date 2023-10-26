5e9b0033ea49f386cc5f794e19636ccd847c7831
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-10-26 19:37:13 -0400

    Update LoggedInName.js

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -4,7 +4,14 @@ import CircularProgressBar from './CircularProgressBar';
 
 function LoggedInName() {
   const [user, setUser] = useState({});
-  const [progress, setProgress] = useState(0); // Add progress state
+  const [progress, setProgress] = useState(0);
+  const [showTaskForm, setShowTaskForm] = useState(false); // Add state for showing the task form
+  const [taskInfo, setTaskInfo] = useState({
+    taskName: '',
+    taskDescription: '',
+    taskDate: '',
+    taskTime: ''
+  });
 
   useEffect(() => {
     let _ud = localStorage.getItem('user_data');
@@ -15,34 +22,73 @@ function LoggedInName() {
   }, []);
 
   const addExperience = () => {
-    // Ollie.
+    // Update progress state
     setProgress(progress + 10);
   };
 
   const addTask = () => {
-    // Julian.
+    // Show the task form when Add Task button is clicked
+    setShowTaskForm(true);
+  };
+
+  const handleTaskFormSubmit = (e) => {
+    e.preventDefault();
+    // Process the task information here
+    console.log('Task Information:', taskInfo);
+    // Close the task form
+    setShowTaskForm(false);
+  };
+
+  const handleTaskInputChange = (e) => {
+    const { name, value } = e.target;
+    setTaskInfo({
+      ...taskInfo,
+      [name]: value
+    });
   };
 
   return (
-<div id='loggedInDiv'>
-  
-<div id="main-task"></div>
-<div id="main-task"></div>
-<div id="main-task"></div>
-<div id="main-task"></div>
-  <span id="userName">{user.name}</span>
-  <CircularProgressBar />
-  <button type="button" id="addExp" className="buttons" onClick={addExperience}>
-    Add Exp
-  </button>
-  <button type="button" id="addTask" className="buttons" onClick={addTask}>
-    Add Task
-  </button>
-</div>
-
-
-
-
+    <div id='loggedInDiv'>
+      <span id="userName">{user.name}</span>
+      <CircularProgressBar />
+      <button type="button" id="addExp" className="buttons" onClick={addExperience}>
+        Add Exp
+      </button>
+      <button type="button" id="addTask" className="buttons" onClick={addTask}>
+        Add Task
+      </button>
+      {showTaskForm && (
+        <form onSubmit={handleTaskFormSubmit}>
+          <input
+            type="text"
+            name="taskName"
+            placeholder="Task Name"
+            value={taskInfo.taskName}
+            onChange={handleTaskInputChange}
+          />
+          <input
+            type="text"
+            name="taskDescription"
+            placeholder="Task Description"
+            value={taskInfo.taskDescription}
+            onChange={handleTaskInputChange}
+          />
+          <input
+            type="date"
+            name="taskDate"
+            value={taskInfo.taskDate}
+            onChange={handleTaskInputChange}
+          />
+          <input
+            type="time"
+            name="taskTime"
+            value={taskInfo.taskTime}
+            onChange={handleTaskInputChange}
+          />
+          <button type="submit">Add</button>
+        </form>
+      )}
+    </div>
   );
 }
 
