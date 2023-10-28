60326e4a4f26bff3056967add71fb2813b2ad7bf
ehymans <ethanhymans@gmail.com>
2023-10-27 21:48:49 -0400

    reverted to previous version

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
-// Importing your components
 import PageTitle from '../components/PageTitle';
 import LoggedInName from '../components/LoggedInName';
-import HomeHeader from '../components/HomeHeader';
+import HomeHeader from '../components/HomeHeader'
 
-// Importing the CSS file
-import '../components/HomePage.css';
 
-const HomePage = () => {
+const HomePage = () =>
+{
     return(
-        <div id="loggedInDiv"> {/* Use an ID or className here if you want to style this specific div */}
+        <div>
             <HomeHeader />
             <LoggedInName />
         </div>
     );
 }
+
 
 export default HomePage;
 
