6c69f24d0d292cdfd84a36abbc60cf82a4b88f13
JulianNutovits <134075623+JulianNutovits@users.noreply.github.com>
2023-10-31 19:03:54 -0400

    linking the form with the endpoint

--- frontend/src/components/LoggedInName.js
+++ frontend/src/components/LoggedInName.js
@@ -32,7 +32,32 @@ function LoggedInName() {
 
   const handleTaskFormSubmit = (e) => {
     e.preventDefault();
-    console.log('Task Information:', taskInfo);
+    // Prepare the data to send to the API
+    const data = {
+      userId: user.userId, // You need to pass the user's ID to the API
+      taskName: taskInfo.taskName,
+      taskDescription: taskInfo.taskDescription,
+      taskDifficulty: taskInfo.taskImportance, // You have taskImportance, but the API expects taskDifficulty
+    };
+
+    // Send an HTTP POST request to the API endpoint
+    fetch('/api/addTask', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        // Handle the API response here (e.g., show a success message or error message)
+        console.log('API Response:', result);
+      })
+      .catch((error) => {
+        // Handle any errors that occur during the fetch.
+        console.error('API Error:', error);
+      });
+
     setShowTaskForm(false);
   };
 
@@ -40,23 +65,21 @@ function LoggedInName() {
     const { name, value } = e.target;
     setTaskInfo({
       ...taskInfo,
-      [name]: value
+      [name]: value,
     });
   };
 
   return (
-    <div class = 'container'>
-    <div id='loggedInDiv'>
-      <span id="userName">{user.name}</span>
-      {/* Basic text */}
-     
-      <h1>Welcome</h1>
-      
-      {/* Add Task button */}
-      <button type="button" id="addTask" className="buttons" onClick={addTask}>
-        Add Task
-      </button>
-      {showTaskForm && (
+    <div className="container">
+      <div id="loggedInDiv">
+        <span id="userName">{user.name}</span>
+        {/* Basic text */}
+        <h1>Welcome</h1>
+        {/* Add Task button */}
+        <button type="button" id="addTask" className="buttons" onClick={addTask}>
+          Add Task
+        </button>
+        {showTaskForm && (
           <form className="task-form" onSubmit={handleTaskFormSubmit}>
             <input
               type="text"
@@ -81,8 +104,8 @@ function LoggedInName() {
             />
             <button type="submit">Add</button>
           </form>
-      )}
-    </div>
+        )}
+      </div>
     </div>
   );
 }
