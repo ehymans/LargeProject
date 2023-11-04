b4c932f393c0cd372d437bb7abdf4504b2e17648
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-04 10:57:30 -0400

    Update DisplayTasks.js

--- frontend/src/components/DisplayTasks.js
+++ frontend/src/components/DisplayTasks.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import './DisplayTasks.css'; // Ensure this path is correct
 
 function DisplayTasks() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
     const _ud = localStorage.getItem('user_data');
-    let userId = '';
     if (_ud) {
       const ud = JSON.parse(_ud);
-      userId = ud.id;
       
-      fetch(`/api/getTaskInfo/${userId}`)
+      fetch(`/api/getTaskInfo/${ud.id}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error(`Network response was not ok: ${response.statusText}`);
@@ -27,26 +26,28 @@ function DisplayTasks() {
   }, []);
 
   return (
-    <div>
-      <h2>Your Tasks</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Task Name</th>
-            <th>Description</th>
-            <th>Importance</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task, index) => (
-            <tr key={index}>
-              <td>{task.taskName}</td>
-              <td>{task.taskDescription}</td>
-              <td>{task.taskImportance}</td>
+    <div className="tasks-container">
+      <h2 className="tasks-header">Your Tasks</h2>
+      <div className="tasks-table-container">
+        <table className="tasks-table">
+          <thead>
+            <tr>
+              <th>Task Name</th>
+              <th>Description</th>
+              <th>Importance</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tasks.map((task, index) => (
+              <tr key={index}>
+                <td>{task.taskName}</td>
+                <td>{task.taskDescription}</td>
+                <td>{task.taskImportance}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
