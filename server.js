0b6a4de923894d0fffb2af233827a597774a3061
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-05 08:02:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path"); // MAY NEED TO DELETE!!
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 const app = express();
 app.use(cors());
