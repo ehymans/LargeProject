1a26d52336013d4d25fa339fd83c7c1200a4d020
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-09 16:07:26 -0500

    Update DisplayTasks.js

--- frontend/src/components/DisplayTasks.js
+++ frontend/src/components/DisplayTasks.js
@@ -156,7 +156,7 @@ function DisplayTasks({ updateTask }) {
             <tr>
               <th>Task Name</th>
               <th>Description</th>
-              <th>Difficulty</th>
+              <th>Importance</th>
               <th>Completed</th>
             </tr>
           </thead>
