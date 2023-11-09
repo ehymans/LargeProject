13e51f982668a0024e761731caf6db17d681b21f
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-09 15:49:28 -0500

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Login.css";
+import "./Login.css"; 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const app_name = "progress-tracker-4331-88c53c23c126";
