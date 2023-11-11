938cfedf2738aa3b6bc8e2709aaf4abc1eab4b9d
Shoaib Farooka <shoaibfarooka@gmail.com>
2023-11-12 01:23:20 +0500

    create task modal added

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "../styles/AddTask.css";
-var bp = require("./Path.js"); 
+import Modal from 'react-modal';
+import { FaTimes } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+var bp = require("./Path.js");
+
+// Set the app element for react-modal
+Modal.setAppElement(document.body);
 
 function LoggedInName({ prevState, setUpdateTask }) {
   const [user, setUser] = useState({});
+  const [IsOpenPopup, setIsOpenPopup] = useState(false);
   const [progress, setProgress] = useState(0);
   const [showTaskForm, setShowTaskForm] = useState(false);
   const [taskInfo, setTaskInfo] = useState({
@@ -18,21 +26,32 @@ function LoggedInName({ prevState, setUpdateTask }) {
     let _ud = localStorage.getItem("user_data");
     if (_ud) {
       let ud = JSON.parse(_ud);
-      console.log(ud.id);
-      console.log(ud.firstName);
-      console.log(ud.lastName);
     } else {
       console.log("ud not found");
     }
   }, []);
 
-  const addExperience = () => {
-    setProgress(progress + 10);
+  //Show Modal when button is clicked
+  const handleClick = (e) => {
+    setIsOpenPopup(true);
   };
 
-  const addTask = () => {
-    setShowTaskForm(true);
+  //Close Modal
+  const handleModalClose = () => {
+    setTaskInfo({
+      taskName: "",
+      taskDescription: "",
+      taskDate: "",
+      taskTime: "",
+      taskImportance: "",
+    });
+    setIsOpenPopup(false);
   };
+
+  // const addExperience = () => {
+  //   setProgress(progress + 10);
+  // };
+
 
   const handleTaskFormSubmit = (e) => {
     e.preventDefault();
@@ -58,9 +77,18 @@ function LoggedInName({ prevState, setUpdateTask }) {
       .then((result) => {
         console.log("API Response:", result);
         setUpdateTask(!prevState);
+        handleModalClose();
+        toast.success('New Task is Added!', {
+          position: 'top-right',
+          autoClose: 5000,
+        });
       })
       .catch((error) => {
         console.error("API Error:", error);
+        toast.error('Unable to create new task. Something went wrong!', {
+          position: 'top-right', // Set the toast position
+          autoClose: 3000, // Close the toast after 3 seconds (optional)
+        });
       });
 
     setShowTaskForm(false);
@@ -75,21 +103,16 @@ function LoggedInName({ prevState, setUpdateTask }) {
   };
 
   return (
-     <div className="container">
-      <div id="loggedInDiv">
-        <span id="userName">{user.name}</span>
-        {/* Basic text */}
-        <h1>Welcome</h1>
-        {/* Add Task button */}
-        <button
-          type="button"
-          id="addTask"
-          className="buttons"
-          onClick={addTask}
-        >
-          Add Task
-        </button>
-        {showTaskForm && (
+    <div className="add-task">
+      <Modal
+        className="modal"
+        overlayClassName="modal-overlay"
+        isOpen={IsOpenPopup}
+        //style={customStyles}
+        onRequestClose={handleModalClose}>
+        <FaTimes size={25} onClick={handleModalClose} className='cross-icon' />
+        <div className="modal-main">
+          <h1>Add Task</h1>
           <form className="task-form" onSubmit={handleTaskFormSubmit}>
             <input
               type="text"
@@ -107,6 +130,7 @@ function LoggedInName({ prevState, setUpdateTask }) {
             />
             <select
               name="taskImportance"
+              style={{ width: '100%' }}
               value={taskInfo.taskImportance}
               onChange={handleTaskInputChange}
             >
@@ -115,12 +139,21 @@ function LoggedInName({ prevState, setUpdateTask }) {
               <option value="Medium">Medium</option>
               <option value="High">High</option>
             </select>
+            <br />
             <button type="submit">Submit Task</button>
           </form>
-        )}
-          
+
+        </div>
+      </Modal>
+
+      <div className="btn-div">
+        <button type="button" className="btn" onClick={handleClick}>
+          Add a New Task
+        </button>
       </div>
-    </div>
+      <ToastContainer />
+
+    </div >
   );
 }
 
