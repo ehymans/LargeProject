b41128ab897f34eb4ae28214fcaa8b051080f623
shadowfrostx <143465828+shadowfrostx@users.noreply.github.com>
2023-11-12 14:50:56 -0500

    Updating app

--- app/lib/api.dart
+++ app/lib/api.dart
@@ -5,6 +5,58 @@ import 'dart:convert';
 
 const String url = "https://progress-tracker-4331-88c53c23c126.herokuapp.com";
 
+Future<int> signIn(String login, String password) async {
+  Map data = {
+    "login": login,
+    "password": password,
+  };
+
+  const Map<String, String> header = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  };
+
+  var jsonData = jsonEncode(data).toString();
+  var response;
+  response = await http.post(Uri.parse("$url/api/login"),
+      body: jsonData, headers: header);
+  if (response.statusCode == 200) {
+    //     print(response.body);
+    // print(response.body.toString() == '{"error":"Login/Password incorrect"}');
+    if (response.body.toString() == '{"error":"Login/Password incorrect"}') {
+      print('Login failed');
+      return 0;
+    } else {
+      print('Login successful');
+      return 1;
+    }
+  } else {
+    print('failed');
+    return 0;
+  }
+}
+
+Future<void> registerUser(
+    String firstName, String lastName, String login, String password) async {
+  Map data = {
+    "first name": firstName,
+    "last name": lastName,
+    "username": login,
+    "password": password
+  };
+  var jsonData = jsonEncode(data);
+  var response = await http.post(Uri.parse("$url/api/register"),
+      body: jsonData, headers: {'Content-type': 'application/json'});
+  print(response.statusCode);
+  print(response.body);
+  if (response.statusCode == 200) {
+    print(data);
+    print('Register successful');
+  } else {
+    print('failed');
+  }
+}
+
 Future<void> addTask(String id, String taskName, String taskDescription,
     String taskDifficulty) async {
   Map data = {
