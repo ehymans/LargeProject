4d79142d10f28ea5bd94ae9ef5cc498ef521b27c
ehymans <ethanhymans@gmail.com>
2023-11-15 16:59:35 -0500

    reformat

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -3,15 +3,13 @@ import React from 'react';
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
 
-const LoginPage = () =>
-{
-
-    return(
-      <div>
-        <PageTitle />
-        <Login />
-      </div>
-    );
+const LoginPage = () => {
+  return (
+    <div>
+      <PageTitle />
+      <Login />
+    </div>
+  );
 };
 
 export default LoginPage;
