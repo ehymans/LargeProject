d400a2e27bf4fb57fb4731c000d8ca254e7f2bd4
ehymans <ethanhymans@gmail.com>
2023-11-16 20:30:04 -0500

    debug websocket

--- api.js
+++ api.js
@@ -313,7 +313,7 @@ exports.setApp = function (app, client, broadcastUpdate) {
       const {userId} = req.body;
       const db = client.db("LargeProject");
       const tasks = await db.collection("Tasks")
-        .find({ UserID: userId, TaskDeleted: {$ne: true} }) // Assuming 'UserID' is the field and TaskDeleted is not true
+        .find({ UserID: userId, TaskCompleted: {$ne: true} }) // Assuming 'UserID' is the field and TaskDeleted is not true
         .toArray();
       
       let tasksInProgress = 0;
