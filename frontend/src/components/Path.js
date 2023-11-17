1b5bf269522419274c84c40c948dff0b3c9e7730
ehymans <ethanhymans@gmail.com>
2023-11-16 20:57:39 -0500

    testing new path

--- frontend/src/components/Path.js
+++ frontend/src/components/Path.js
@@ -1,10 +1,12 @@
-const app_name = 'progress-tracker-4331-88c53c23c126'
+//const app_name = 'progress-tracker-4331-88c53c23c126'
+const app_name = 'Dare2Do'
 exports.buildPath =
 function buildPath(route)
 {
 if (process.env.NODE_ENV === 'production')
 {
-return 'https://' + app_name + '.herokuapp.com/' + route;
+//return 'https://' + app_name + '.herokuapp.com/' + route;
+return 'https://' + app_name + '.online/' + route;
 }
 else
 {
