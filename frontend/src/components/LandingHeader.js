1cd7a67dbfe9b22b06086518dedf97799dd3898e
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-20 17:47:37 -0500

    Add files via upload

---
+++ frontend/src/components/LandingHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link for navigation
+import '../styles/PageTitle.css';
+
+function LandingHeader() {
+  return (
+    <div className="page-title">
+      <h1 id="title">Dare2Do</h1>
+      
+    </div>
+  );
+}
+
+export default LandingHeader;
