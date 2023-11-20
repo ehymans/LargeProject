e9be3cba8ad61f6e89b4b671b313def41a5bf308
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-20 12:34:43 -0500

    Add files via upload

---
+++ frontend/src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+import Landing from '../components/Landing';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <PageTitle />
+      <Landing />
+    </div>
+  );
+};
+
+export default LandingPage;
