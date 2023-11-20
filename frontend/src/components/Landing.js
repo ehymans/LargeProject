a81536e84adc67ea8a00dad5081157bca85f2862
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-20 12:34:56 -0500

    Add files via upload

---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,23 @@
+// Import necessary modules and components
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Landing.css';
+
+// Landing page component
+function Landing() {
+    return (
+        <div class="landing-container">
+        <div class="landing-content">
+          <h1>Welcome to Dare2DO</h1>
+          <p>With our gameified task manager, finding motivation to get things done has never been easier.</p>
+          <div class="landing-actions">
+            <a href="#" class="landing-button">Log In</a>
+            <a href="#" class="landing-button">Sign Up</a>
+          </div>
+        </div>
+      </div>
+      
+    );
+}
+
+export default Landing;
