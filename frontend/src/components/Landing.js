2d8df989c21af03a85e9a093c9f7e648f83e4e02
cbowerfind <123131082+cbowerfind@users.noreply.github.com>
2023-11-26 12:09:04 -0500

    Update Landing.js

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -8,7 +8,7 @@ function Landing() {
     return (
         <div class="landing-container">
         <div class="landing-content">
-          <h1>Welcome to Dare2DO</h1>
+          <h1>Welcome to Dare2Do</h1>
           <p>With our gamified task manager, finding motivation to get things done has never been easier.</p>
           <div class="landing-actions">
             <a href="/login" class="landing-button">Log In</a>
